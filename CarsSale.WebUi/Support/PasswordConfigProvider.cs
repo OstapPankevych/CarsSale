4aab16aa4bf7aeb4a3c4c777dcdcbaf7f562ca9e
OstapPankevychEpam <ostap_pankevych@epam.com>
2017-12-06 18:46:20 +0200

    improved config reading with static class

--- CarsSale.WebUi/Support/PasswordConfigProvider.cs
+++ CarsSale.WebUi/Support/PasswordConfigProvider.cs
@@ -1,23 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Linq;
-using System.Web;
+﻿using System.Configuration;
 
 namespace CarsSale.WebUi.Support
 {
     public static class PasswordConfigProvider
     {
-        private static PasswordConfig _config;
-        private static PasswordConfig Config
-        {
-            get
-            {
-                if (_config != null) return _config;
-                _config = (PasswordConfig) ConfigurationManager.GetSection("PasswordConfig");
-                return _config;
-            }
-        }
+        private static PasswordConfig Config => (PasswordConfig) ConfigurationManager.GetSection("PasswordConfig");
 
         public static int RequiredLength => int.Parse(Config.RequiredLength);
         public static bool RequireNonLetterOrDigit => bool.Parse(Config.RequireNonLetterOrDigit);
